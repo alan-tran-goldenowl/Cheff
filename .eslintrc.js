ea6c650c094116f6db61b30b438be6aa4f956666
Macintosh <dotrunghieu7696@gmail.com>
2018-07-26 14:32:23 +0700

    re config eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,14 @@
 module.exports = {
-    "extends": "airbnb"
+  env: {
+    node: true,
+    es6: true
+  },
+  parser: "babel-eslint",
+  plugins: ["react", "react-native", "jsx-a11y", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "no-unexpected-multiline": "error",
+    "react/jsx-filename-extension": [0]
+  },
+  extends: "airbnb"
 };
