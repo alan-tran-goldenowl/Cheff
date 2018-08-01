f4861e8ae58a8d72d08508b083b39ad925d8c1a8
Macintosh <dotrunghieu7696@gmail.com>
2018-08-01 14:42:32 +0700

    re config structure of calendar component

--- screens/MealPlanScreen.js
+++ screens/MealPlanScreen.js
@@ -7,10 +7,7 @@ import {
   TouchableOpacity,
   StyleSheet
 } from "react-native";
-import moment from "moment";
-import { Calendar, LocaleConfig } from "react-native-calendars";
-
-import CustomArrow from "../components/CustomArrow";
+import MyCalendar from "../components/Calendar";
 
 export default class MealPlan extends Component {
   static navigationOptions = {
@@ -20,94 +17,8 @@ export default class MealPlan extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      fullname: "Michael",
-      currentMonth: moment()
+      fullname: "Michael"
     };
-
-    this.loadCustomNameCalendar();
-    this.themeCalendar = {
-      "stylesheet.calendar.header": {
-        header: {
-          flexDirection: "row",
-          justifyContent: "space-between",
-          paddingLeft: 10,
-          paddingRight: 10,
-          alignItems: "center",
-          borderBottomWidth: 1,
-          borderTopWidth: 1,
-          borderColor: "#ddd"
-        },
-        dayHeader: {
-          marginTop: 2,
-          marginBottom: 7,
-          width: 32,
-          textAlign: "center",
-          color: "black"
-        }
-      },
-      "stylesheet.day.basic": {
-        dot: {
-          width:6,
-          height: 6,
-          marginTop: -30,
-          marginLeft: 35,
-          borderRadius: 3,
-          opacity: 1
-        },
-        selectedDot: {
-          backgroundColor: 'red'
-        },
-      }
-    };
-  }
-
-  loadCustomNameCalendar() {
-    // custom text month and day on calendar
-    LocaleConfig.locales["en"] = {
-      monthNames: [
-        "JANUARY",
-        "FEBRUARY",
-        "MARCH",
-        "APRIL",
-        "MAY",
-        "JUNE",
-        "JULY",
-        "AUGUST",
-        "SEPTEMBER",
-        "OCTOBER",
-        "NOVEMBER",
-        "DECEMBER"
-      ],
-      dayNamesShort: ["S", "M", "T", "W", "T", "F", "S"]
-    };
-
-    LocaleConfig.defaultLocale = "en";
-  }
-
-  loadMarkedDates(dates) {
-    const currentDateKey = dates.format("YYYY-MM-DD");
-    const markedDates = {
-      [currentDateKey]: {
-        selected: true,
-        marked: true,
-        selectedColor: "black",
-        dotColor: "red"
-      },
-      "2018-08-05": {
-        //  selected: true,
-        marked: true,
-        //  selectedColor: "black",
-        dotColor: "red"
-      },
-      "2018-08-03": {
-        selected: true,
-        //  marked: true,
-        selectedColor: "black"
-        //  dotColor: "red"
-      }
-    };
-
-    return markedDates;
   }
 
   renderHeader() {
@@ -126,13 +37,11 @@ export default class MealPlan extends Component {
             />
           </TouchableOpacity>
         </View>
-
         <View
           style={{ flex: 7, justifyContent: "center", alignItems: "center" }}
         >
           <Text style={{ fontSize: 16 }}>Meal Plan</Text>
         </View>
-
         <View
           style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
         >
@@ -185,44 +94,9 @@ export default class MealPlan extends Component {
   }
 
   renderCalendar() {
-    const { currentMonth } = this.state;
-    const markedDates = this.loadMarkedDates(currentMonth);
-
     return (
       <View style={{ marginTop: 15 }}>
-        <Calendar
-          firstDay={1}
-          style={{ width: "100%", paddingLeft: 0, paddingRight: 0 }}
-          theme={this.themeCalendar}
-          monthFormat={"MMMM, yyyy"}
-          current={currentMonth.toDate()}
-          //  markingType={"custom"}
-          markedDates={markedDates}
-          //handle press day
-          onDayPress={day => {
-            console.log("selected day", day);
-          }}
-          // Handler which gets executed on day long press. Default = undefined
-          onDayLongPress={day => {
-            console.log("selected day", day);
-          }}
-          // Replace default arrows with custom ones (direction can be 'left' or 'right')
-          renderArrow={direction => (
-            <CustomArrow direction={direction} currentMonth={currentMonth} />
-          )}
-          onPressArrowLeft={substractMonth => {
-            substractMonth();
-            this.setState(prevState => {
-              currentMonth: prevState.currentMonth.subtract(1, "months");
-            });
-          }}
-          onPressArrowRight={addMonth => {
-            addMonth();
-            this.setState(prevState => {
-              currentMonth: prevState.currentMonth.add(1, "months");
-            });
-          }}
-        />
+        <MyCalendar />
       </View>
     );
   }
@@ -273,10 +147,4 @@ const styles = StyleSheet.create({
   infomationFood: {
     flex: 1
   }
-  // calendar: {
-  //   borderWidth: 1
-  // },
-  // theme: {
-  // //  arrowColor: "orange"
-  // }
 });
