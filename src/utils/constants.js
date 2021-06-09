2efcc8b2f14a93ce1547c2e54df1c55860895c61
lawson-dev <lawson.nguyen.goldenowl@gmail.com>
2021-06-09 12:05:30 +0700

    optimize config switch navigation

---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+export const ROUTE_NAME = {
+  Main: 'Main',
+  Links: 'Links',
+  Search: 'Search',
+  ListPlan: 'ListPlan',
+  Settings: 'Settings',
+  CreatePlan: 'CreatePlan',
+  FoodDetail: 'FoodDetail',
+  EditProfile: 'EditProfile',
+  PlanDetails: 'PlanDetails',
+  TodoPlanDetail: 'TodoPlainDetail',
+  CreatePlanToBuy: 'CreatePlanToBuy',
+  PrivacyAndPolicy: 'PrivacyAndPolicy',
+}
